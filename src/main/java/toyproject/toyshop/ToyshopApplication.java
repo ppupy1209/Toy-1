013e8166db0904ad78a37c5a04923defc851c523
yeonwoo <ppupy1209@naver.com>
2023-09-09 15:40:51 +0900

    초기 세팅

---
+++ src/main/java/toyproject/toyshop/ToyshopApplication.java
@@ -0,0 +1,13 @@
+package toyproject.toyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/toyproject/toyshop/ToyshopApplicationTests.java
@@ -0,0 +1,13 @@
+package toyproject.toyshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
