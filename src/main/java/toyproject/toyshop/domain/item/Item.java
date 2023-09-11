64261ccd70f0a4c214304e888213a25716cf202a
yeonwoo <ppupy1209@naver.com>
2023-09-12 01:19:51 +0900

    상품 엔티티에 비즈니스 로직 추가

--- src/main/java/toyproject/toyshop/domain/item/Item.java
+++ src/main/java/toyproject/toyshop/domain/item/Item.java
@@ -3,6 +3,7 @@ package toyproject.toyshop.domain.item;
 import lombok.Getter;
 import lombok.Setter;
 import toyproject.toyshop.domain.Category;
+import toyproject.toyshop.exception.NotEnoughStockException;
 
 import javax.persistence.*;
 import javax.print.attribute.standard.MediaSize;
@@ -12,7 +13,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter @Setter
+@Getter
 public abstract class Item {
 
     @Id @GeneratedValue
@@ -25,4 +26,21 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    /**
+     * 재고 수량 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+    /**
+     * 재고 수량 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock<0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
 }
