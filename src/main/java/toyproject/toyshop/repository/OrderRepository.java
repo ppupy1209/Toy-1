04881b479e5090eae977940912ed61518084c729
yeonwoo <ppupy1209@naver.com>
2023-09-12 14:59:38 +0900

    OrderRepository 구현

---
+++ src/main/java/toyproject/toyshop/repository/OrderRepository.java
@@ -0,0 +1,29 @@
+package toyproject.toyshop.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import toyproject.toyshop.domain.Order;
+import toyproject.toyshop.domain.OrderItem;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+//    public List<Order> findAll(OrderSearch orderSearch) {
+//
+//    }
+    
+}
