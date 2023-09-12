c71cd4634fb2f10ed6e67ca662dfb727b80e9b98
yeonwoo <ppupy1209@naver.com>
2023-09-12 21:20:32 +0900

    OrderSearch 구현

---
+++ src/main/java/toyproject/toyshop/repository/OrderSearch.java
@@ -0,0 +1,12 @@
+package toyproject.toyshop.repository;
+
+import lombok.Getter;
+import lombok.Setter;
+import toyproject.toyshop.domain.OrderStatus;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName; // 회원 이름
+    private OrderStatus orderStatus; // 주문 상태 [ORDER, CANCEL]
+}
