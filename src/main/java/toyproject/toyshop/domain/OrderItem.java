a39b4465ff6358528261447d73c8de6dfb63e4d8
yeonwoo <ppupy1209@naver.com>
2023-09-12 19:36:03 +0900

    기본 생성자 추가

--- src/main/java/toyproject/toyshop/domain/OrderItem.java
+++ src/main/java/toyproject/toyshop/domain/OrderItem.java
@@ -1,6 +1,8 @@
 package toyproject.toyshop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import toyproject.toyshop.domain.item.Item;
 
@@ -10,6 +12,7 @@ import static javax.persistence.FetchType.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
