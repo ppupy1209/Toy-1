c4ccd83b71ad3e7adc248753b0261da6460947ef
yeonwoo <ppupy1209@naver.com>
2023-09-13 18:00:47 +0900

    상품 수정 구현

--- src/main/java/toyproject/toyshop/service/ItemService.java
+++ src/main/java/toyproject/toyshop/service/ItemService.java
@@ -3,6 +3,7 @@ package toyproject.toyshop.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.toyshop.domain.item.Book;
 import toyproject.toyshop.domain.item.Item;
 import toyproject.toyshop.repository.ItemRepository;
 
@@ -17,6 +18,14 @@ public class ItemService {
 
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(price);
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+
     }
 
     @Transactional(readOnly = true)
