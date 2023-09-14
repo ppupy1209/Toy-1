91b461834c3fd7b40bd514490534d0e84ca79a6e
yeonwoo <ppupy1209@naver.com>
2023-09-14 11:58:57 +0900

    회원 수정 API 구현

--- src/main/java/toyproject/toyshop/api/MemberApiController.java
+++ src/main/java/toyproject/toyshop/api/MemberApiController.java
@@ -1,10 +1,9 @@
 package toyproject.toyshop.api;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import toyproject.toyshop.domain.Member;
 import toyproject.toyshop.service.MemberService;
 
@@ -23,6 +22,26 @@ public class MemberApiController {
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+       memberService.update(id,request.getName());
+       Member findMember = memberService.findOne(id);
+       return new UpdateMemberResponse(findMember.getId(), findMember.getName());
     }
 
     @Data
