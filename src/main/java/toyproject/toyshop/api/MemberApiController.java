d2e9270aecd00a62fd67b9e962a2cb152cec0d91
yeonwoo <ppupy1209@naver.com>
2023-09-14 12:25:13 +0900

    회원 조회 API 구현

--- src/main/java/toyproject/toyshop/api/MemberApiController.java
+++ src/main/java/toyproject/toyshop/api/MemberApiController.java
@@ -8,6 +8,8 @@ import toyproject.toyshop.domain.Member;
 import toyproject.toyshop.service.MemberService;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,13 +17,44 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    @PostMapping("/api/v1/members")
+    @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMember(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
 
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id,request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName());
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
     }
 
     @Data
@@ -36,13 +69,7 @@ public class MemberApiController {
         private String name;
     }
 
-    @PutMapping("/api/v2/members/{id}")
-    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
-                                               @RequestBody @Valid UpdateMemberRequest request) {
-       memberService.update(id,request.getName());
-       Member findMember = memberService.findOne(id);
-       return new UpdateMemberResponse(findMember.getId(), findMember.getName());
-    }
+    
 
     @Data
     static class CreateMemberRequest {
